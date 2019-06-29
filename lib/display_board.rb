27c14850cf0e12651df17365ef6784e41f80f681
Sanima Shrestha <saneemastha@gmail.com>
2019-06-29 14:03:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
  def display_board(board)
-       display_board=puts".... ..... ...."
-              puts "#{board[3]}|#{board[4]}|#{board[5]}"
-                 puts".... ..... ...."
-       puts "#{board[6]}|#{board[7]}|#{board[8]}"
-          puts".... ..... ...."
+       display_board=
+                    
+                    puts".... ..... ...."
+                    puts "#{board[3]}|#{board[4]}|#{board[5]}"
+                    puts".... ..... ...."
+                    puts "#{board[6]}|#{board[7]}|#{board[8]}"
+                    puts".... ..... ...."
 end
 board=["","","","","X","0","","","",""]
 board=[" "," "," "," "," "," "," "," "," "," "]
